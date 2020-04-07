6951d198a991cfa26d0a8f301d42594807f41eee
lifan <2224779926@qq.com>
2020-04-07 15:03:07 +0800

    fix

--- src/main/java/com/example/face/FaceApplication.java
+++ src/main/java/com/example/face/FaceApplication.java
@@ -1,8 +1,11 @@
 package com.example.face;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -11,6 +14,14 @@ public class FaceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FaceApplication.class, args);
+    }
+
+
+    @Bean(destroyMethod = "close", initMethod = "init")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DruidDataSource druidDataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        return  druidDataSource;
     }
 
 }
